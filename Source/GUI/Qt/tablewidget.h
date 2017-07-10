cfdd00dc4071e4f253aa77a5101c09f575c4bf84
Jérôme Martinez <jerome@mediaarea.net>
2017-07-10 22:36:24 +0200

    Explicit license

--- Source/GUI/Qt/tablewidget.h
+++ Source/GUI/Qt/tablewidget.h
@@ -1,3 +1,9 @@
+/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
+*
+*  Use of this source code is governed by a MIT-style license that can
+*  be found in the License.html file in the root of the source tree.
+*/
+
 #ifndef TABLEWIDGET_H
 #define TABLEWIDGET_H
 

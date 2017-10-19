42d3c8802100c2f3d60179a8f20695cd3709107a
Maxime Gervais <gervais.maxime@gmail.com>
2017-10-19 14:34:52 +0200

    Check for new version
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Source/GUI/Qt/mainwindow.h
+++ Source/GUI/Qt/mainwindow.h
@@ -10,6 +10,7 @@
 #include <QTableWidgetItem>
 #include <QMainWindow>
 #include <QMenu>
+#include <QNetworkAccessManager>
 
 #include "core.h"
 
@@ -29,11 +30,17 @@ private:
     void dragEnterEvent(QDragEnterEvent* Event);
     void dropEvent(QDropEvent* Event);
     void Update_Table();
+#if !defined(MOVMETAEDIT_NO_VERSION_CHECK)
+    void CheckUpdate();
+#endif
 
     Ui::MainWindow* Ui;
     QMenu* Context_Menu;
 
     Core* C;
+#if !defined(MOVMETAEDIT_NO_VERSION_CHECK)
+    QNetworkAccessManager* CheckUpdate_Handle;
+#endif
 
 protected:
     void closeEvent(QCloseEvent *event);
@@ -47,8 +54,12 @@ private slots:
     void on_Menu_File_Close_triggered();
     void on_Menu_File_Close_All_triggered();
     void on_Table_Widget_itemSelectionChanged();
+    void on_Menu_New_Version_triggered();
     void Show_Context_Menu(const QPoint&);
     void Table_Widget_Changed();
+#if !defined(MOVMETAEDIT_NO_VERSION_CHECK)
+    void CheckUpdateReceived(QNetworkReply*);
+#endif
 };
 
 #endif // MAINWINDOW_H
